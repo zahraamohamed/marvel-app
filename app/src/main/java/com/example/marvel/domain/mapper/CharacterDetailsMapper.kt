3b7445b2eca506f32e02edee6971abdea6c1664d
zahraa resen <zaawory98@gmail.com>
2021-12-06 23:00:05 +0300

    edit character mapper

--- app/src/main/java/com/example/marvel/domain/mapper/CharacterDetailsMapper.kt
+++ app/src/main/java/com/example/marvel/domain/mapper/CharacterDetailsMapper.kt
@@ -2,17 +2,19 @@ package com.example.marvel.domain.mapper
 
 import com.example.marvel.data.remote.response.CharactersDto
 import com.example.marvel.domain.models.Character
+import com.example.marvel.domain.models.CharacterDetails
 
-class CharacterDetailsMapper : MapToCharacter<CharactersDto, Character> {
+class CharacterDetailsMapper : MapToCharacter<CharactersDto, CharacterDetails> {
 
-    override fun mapToCharacter(input: CharactersDto): Character {
+    override fun mapToCharacter(input: CharactersDto): CharacterDetails {
 
         val url = input.thumbnail?.path?.replace("http", "https")
-        return Character(
+        return CharacterDetails(
             id = input.id,
             name = input.name,
             imageUrl = "${url}.${input.thumbnail?.extension}",
             description = input.description,
+            stories = input.stories,
         )
     }
 }
---
+++ app/src/main/java/com/example/marvel/domain/models/CharacterDetails.kt
@@ -0,0 +1,12 @@
+package com.example.marvel.domain.models
+
+import com.example.marvel.data.remote.response.StoriesDto
+import com.example.marvel.data.remote.response.SubList
+
+data class CharacterDetails (
+    val id: Int?,
+    val name: String?,
+    val imageUrl: String?,
+    val description:String?,
+    val stories:SubList?,
+)
